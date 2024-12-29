02fc2c576290dfbbdf8fbdc466292ccd362fb396
Trsh16 <94509391+Trsh16@users.noreply.github.com>
2024-12-29 23:01:14 +0700

    Unit Testing on Adding Member

--- PerpustakaanUITests/PerpustakaanUITests.swift
+++ PerpustakaanUITests/PerpustakaanUITests.swift
@@ -9,35 +9,53 @@ import XCTest
 
 final class PerpustakaanUITests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func setUpWithError() throws {
+        continueAfterFailure = false
     }
 
     @MainActor
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testAddMemberForm() throws {
         let app = XCUIApplication()
         app.launch()
 
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+        let launchScreenAttachment = XCTAttachment(screenshot: app.screenshot())
+        launchScreenAttachment.name = "Launch Screen"
+        launchScreenAttachment.lifetime = .keepAlways
+        add(launchScreenAttachment)
 
-    @MainActor
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
+        let addNewMemberButton = app.buttons["Add New Member"]
+        XCTAssertTrue(addNewMemberButton.exists, "The 'Add New Member' button should exist")
+        addNewMemberButton.tap()
+
+        let addMemberTitle = app.staticTexts["Add Member"]
+        XCTAssertTrue(addMemberTitle.exists, "The AddMemberView screen should be displayed after tapping 'Add New Member'")
+
+        let nameTextField = app.textFields["Member Name"]
+        XCTAssertTrue(nameTextField.exists, "The 'Member Name' text field should exist")
+        nameTextField.tap()
+        nameTextField.typeText("Trisha")
+
+        let addressTextField = app.textFields["Member Address"]
+        XCTAssertTrue(addressTextField.exists, "The 'Member Address' text field should exist")
+        addressTextField.tap()
+        addressTextField.typeText("Jln. Dharmahusada")
+
+        let addMemberButton = app.buttons["Add Member"]
+        XCTAssertTrue(addMemberButton.exists, "The 'Add Member' button should exist")
+        addMemberButton.tap()
+
+        let successAlert = app.alerts["Member Added"]
+        XCTAssertTrue(successAlert.exists, "The success alert should appear after adding a member")
+
+        successAlert.buttons["OK"].tap()
+
+        let addMemberFormScreenshot = XCTAttachment(screenshot: app.screenshot())
+        addMemberFormScreenshot.name = "Add Member Form After Success"
+        addMemberFormScreenshot.lifetime = .keepAlways
+        add(addMemberFormScreenshot)
     }
 }
